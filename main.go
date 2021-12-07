b2263986b605e05f4b73c6bafc5f43f022775af0
zhaowenqiang <zhaowenqiang@offcn.com>
2021-12-07 15:54:11 +0800

    first version

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	r := gin.Default()
+	r.MaxMultipartMemory = 8 << 20 // 20MB
+	r.Static("/", "./public")
+
+	fmt.Println(getFileName(".pdf"))
+	r.POST("/api/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Multipart form
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+		files := form.File["files"]
+
+		for _, file := range files {
+			ext := filepath.Ext(file.Filename)
+			if ext != ".pdf" {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", "file ext is not allowed!"))
+				return
+			}
+
+			if err := c.SaveUploadedFile(file, getFileName(ext)); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+				return
+			}
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email))
+	})
+
+	r.Run(":8080")
+}
+
+func getFileName(ext string) string {
+
+	const UPLOAD_BASE_DIR = "public/upload/"
+	// 时间格式必须按照这个来： RFC3339     = "2006-01-02T15:04:05Z07:00"
+	datetime := time.Now().Format("20060102")
+
+	dstPath := UPLOAD_BASE_DIR + datetime + "/"
+	dstFile := dstPath + uuid.New().String() + ext
+
+	_, err := os.Stat(dstPath)
+	res := os.IsNotExist(err)
+	if res == true {
+		os.MkdirAll(dstPath, os.ModePerm)
+	}
+
+	return dstFile
+}
